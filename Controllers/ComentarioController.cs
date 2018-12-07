ca8c34614d047d3dcc823dd689e9d4144a78e3cf
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-12-07 11:33:41 -0200

    Acabou o tempo de novo

--- Controllers/ComentarioController.cs
+++ Controllers/ComentarioController.cs
@@ -1,7 +1,11 @@
-namespace Senai.Projeto.Final.Final.web.Mvc.Controllers
-{
-    public class ComentarioController
-    {
-        
+using Microsoft.AspNetCore.Mvc;
+
+namespace Senai.Projeto.Final.Final.web.Mvc.Controllers {
+    public class ComentarioController : Controller {
+        [HttpGet]
+        public IActionResult Cadastrar () {
+        return View (); 
+        }
+
     }
 }
